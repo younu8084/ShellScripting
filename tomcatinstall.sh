ea2cc4092ec91103e636b83b0e485912d4aebe2c
MaheedharTalluri <1210815148@gitam.in>
2019-12-04 18:40:45 +0530

    Final Tomcat.sh

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -3,5 +3,7 @@
 wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
 #unzipping tar file 
 tar -xvzf apache-tomcat-8.5.49.tar.gz
+#remove tomcat downloaded tar file
+rm -rf apache-tomcat-8.5.49.tar.gz
 #running tomcat
 apache-tomcat-8.5.49/bin/./startup.sh
