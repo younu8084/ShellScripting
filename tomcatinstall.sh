9c174ed46c6c3ae5c5f3b5c422a0b631b2f98b19
MaheedharTalluri <1210815148@gitam.in>
2019-12-04 19:02:33 +0530

    Changing the port

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -5,5 +5,7 @@ wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.
 tar -xvzf apache-tomcat-8.5.49.tar.gz
 #remove tomcat downloaded tar file
 rm -rf apache-tomcat-8.5.49.tar.gz
+#changing the port for tomcat
+sed -i 's/port="8080"/port="8880"/' apache-tomcat-8.5.49/conf/server.xml
 #running tomcat
 apache-tomcat-8.5.49/bin/./startup.sh
