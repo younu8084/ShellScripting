27e8c43630997cc8cb6653577bae3f6e05983018
MaheedharTalluri <1210815148@gitam.in>
2019-12-04 18:39:09 +0530

    Comments Added

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -1,9 +1,7 @@
-mkdir /home/ec2-user/apache
-
-cd /home/ec2-user/apache/
-
+#Go to the folder in which you want to install tomcat
+#downloading tomcat
 wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
-
-
-
+#unzipping tar file 
 tar -xvzf apache-tomcat-8.5.49.tar.gz
+#running tomcat
+apache-tomcat-8.5.49/bin/./startup.sh
