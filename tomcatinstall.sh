44eeace6937b6893813a0b9bd46ae3e3f17c6b6d
MaheedharTalluri <1210815148@gitam.in>
2019-12-04 18:26:44 +0530

    Tomcat corrected

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -1,7 +1,9 @@
 mkdir /home/ec2-user/apache
 
+cd /home/ec2-user/apache/
+
 wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
 
-cd /home/ec2-user/apache/
+
 
 tar -xvzf apache-tomcat-8.5.49.tar.gz
