924a8607ab3baacea24141820fc4b37890680d6c
MaheedharTalluri <1210815148@gitam.in>
2019-12-05 12:46:28 +0530

    java installed

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -1,4 +1,8 @@
+
+
 #Go to the folder in which you want to install tomcat
+#installing java
+sudo yum install java
 #downloading tomcat
 wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
 #unzipping tar file 
