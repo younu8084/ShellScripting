1c60212342d7011ce98d8e7273f4cabc26e87c5d
MaheedharTalluri <1210815148@gitam.in>
2019-12-06 13:07:50 +0530

    user testing

--- user.sh
+++ user.sh
@@ -1,2 +1,56 @@
-read var
-echo 'this is $var' >> text.txt
+
+
+#Go to the folder in which you want to install tomcat
+#installing java
+sudo yum install java
+
+
+
+#downloading tomcat
+wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
+
+
+
+#unzipping tar file 
+tar -xvzf apache-tomcat-8.5.49.tar.gz
+
+
+read -p "do you want to delete the zipped file [enter y/n]:" var1
+
+y=$(echo $var1 | tr -s '[:upper:]' '[:lower:]')
+
+if [["$var1" = "y"]]; then
+
+#remove tomcat downloaded tar file
+rm -rf apache-tomcat-8.5.49.tar.gz
+fi
+
+read -p "Do you want to change port for tomcat [enter y/n]:" var2
+
+y=$(echo $var2 | tr -s '[:upper:]' '[:lower:]')
+
+if [["$var2" = "y"]; then
+read -p "Enter the new port:" port
+#changing the port for tomcat
+sed -i "s/port="8080"/port="$port"/" apache-tomcat-8.5.49/conf/server.xml
+fi
+#creating users in tomcat-users.xml
+
+sed -i 's\</tomcat-users>\<!-- -->\g' apache-tomcat-8.5.49/conf/tomcat-users.xml
+
+echo '<role rolename="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="admin" password="admin" roles="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<role rolename="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="script" password="script" roles="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '</tomcat-users>' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+
+#setting valve in comments
+
+sed -i 's/<Valve /<!-- <Valve /' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+
+sed -i 's\:1" />\:1" /> -->\g' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+
+
+
+#running tomcat
+apache-tomcat-8.5.49/bin/./startup.sh
