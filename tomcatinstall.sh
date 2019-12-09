68d0056d3643ed1c1d2b632ff080c9c5c10ab792
younu8084 <55754022+younu8084@users.noreply.github.com>
2019-12-09 16:54:42 +0530

    Update tomcatinstall.sh

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -10,16 +10,16 @@ tar -xvzf apache-tomcat-8.5.49.tar.gz
 #remove tomcat downloaded tar file
 rm -rf apache-tomcat-8.5.49.tar.gz
 #changing the port for tomcat
-sed -i 's/port="8080"/port="8880"/' apache-tomcat-8.5.49/conf/server.xml
+sed -i 's/port="8080"/port="8090"/' apache-tomcat-8.5.49/conf/server.xml
 
 #creating users in tomcat-users.xml
 
 sed -i 's\</tomcat-users>\<!-- -->\g' apache-tomcat-8.5.49/conf/tomcat-users.xml
 
 echo '<role rolename="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
-echo '<user username="admin" password="admin" roles="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="tomcat" password="admin" roles="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
 echo '<role rolename="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
-echo '<user username="script" password="script" roles="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<user username="tomcat" password="admin" roles="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
 echo '</tomcat-users>' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
 
 #setting valve in comments
